41fc8476f2d87b71db11f86833fe6eb0bffd68ef
Joseph-André Turk <josephandre.turk@zama.ai>
2024-04-23 18:20:26 +0200

    chore: updated to use fhevmjs 0.4.0

--- app/components/Devnet/Devnet.tsx
+++ app/components/Devnet/Devnet.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react';
-import { getInstance } from '../../fhevmjs';
+import { useEffect, useState } from "react";
+import { getInstance } from "../../fhevmjs";
 
-import './Devnet.css';
+import "./Devnet.css";
 
-const toHexString = (bytes: Uint8Array) => bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+const toHexString = (bytes: Uint8Array) =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
 
 export const Devnet = () => {
   const [encryption, setEncryption] = useState<Uint8Array>();
-  const [token, setToken] = useState<ReturnType<typeof instance.generateToken>>();
+  const [token, setToken] =
+    useState<ReturnType<typeof instance.generatePublicKey>>();
   const instance = getInstance();
 
   useEffect(() => {
     setEncryption(instance.encrypt32(1337));
     setToken(
-      instance.generateToken({
-        verifyingContract: '0x309cf2aae85ad8a1db70ca88cfd4225bf17a7482',
+      instance.generatePublicKey({
+        verifyingContract: "0x309cf2aae85ad8a1db70ca88cfd4225bf17a7482",
       })
     );
   }, [instance]);
@@ -24,11 +26,15 @@ export const Devnet = () => {
       <dl>
         <dt className="Devnet__title">This is an encryption of 1337:</dt>
         <dd className="Devnet__dd">
-          <pre className="Devnet__pre">{encryption && toHexString(encryption)}</pre>
+          <pre className="Devnet__pre">
+            {encryption && toHexString(encryption)}
+          </pre>
         </dd>
         <dt className="Devnet__title">And this is a EIP-712 token</dt>
         <dd className="Devnet__dd">
-          <pre className="Devnet__pre">{token && JSON.stringify(token.token)}</pre>
+          <pre className="Devnet__pre">
+            {token && JSON.stringify(token.eip712)}
+          </pre>
         </dd>
       </dl>
     </div>
