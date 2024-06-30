150e5b543237dbb877d303928989421af73013c4
Joseph-André Turk <josephandre.turk@zama.ai>
2024-06-30 23:56:07 +0200

    feat: updated to fhevmjs 0.5

--- app/components/Devnet/Devnet.tsx
+++ app/components/Devnet/Devnet.tsx
@@ -7,18 +7,28 @@ const toHexString = (bytes: Uint8Array) =>
   bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
 
 export const Devnet = () => {
+  const [handles, setHandles] = useState<Uint8Array[]>([]);
   const [encryption, setEncryption] = useState<Uint8Array>();
-  const [token, setToken] =
-    useState<ReturnType<typeof instance.generatePublicKey>>();
+  const [eip712, setEip712] =
+    useState<ReturnType<typeof instance.createEIP712>>();
   const instance = getInstance();
 
   useEffect(() => {
-    setEncryption(instance.encrypt32(1337));
-    setToken(
-      instance.generatePublicKey({
-        verifyingContract: "0x309cf2aae85ad8a1db70ca88cfd4225bf17a7482",
-      })
+    const { handles, inputProof } = instance
+      .createEncryptedInput(
+        "0x309cf2aae85ad8a1db70ca88cfd4225bf17a7456",
+        "0x309cf2aae85ad8a1db70ca88cfd4225bf17a7482"
+      )
+      .add64(32)
+      .encrypt();
+    setHandles(handles);
+    setEncryption(inputProof);
+    const { publicKey } = instance.generateKeypair();
+    const eip = instance.createEIP712(
+      publicKey,
+      "0x309cf2aae85ad8a1db70ca88cfd4225bf17a7482"
     );
+    setEip712(eip);
   }, [instance]);
 
   return (
@@ -27,14 +37,15 @@ export const Devnet = () => {
         <dt className="Devnet__title">This is an encryption of 1337:</dt>
         <dd className="Devnet__dd">
           <pre className="Devnet__pre">
-            {encryption && toHexString(encryption)}
+            Handle: {handles.length && toHexString(handles[0])}
+          </pre>
+          <pre className="Devnet__pre">
+            Input Proof: {encryption && toHexString(encryption)}
           </pre>
         </dd>
         <dt className="Devnet__title">And this is a EIP-712 token</dt>
         <dd className="Devnet__dd">
-          <pre className="Devnet__pre">
-            {token && JSON.stringify(token.eip712)}
-          </pre>
+          <pre className="Devnet__pre">{eip712 && JSON.stringify(eip712)}</pre>
         </dd>
       </dl>
     </div>
