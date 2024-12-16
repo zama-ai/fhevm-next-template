c6171d0f7116b024aab459ffc556707dddb0a43e
Clément 'birdy' Danjou <clement@danjou.io>
2024-12-16 19:30:56 +0100

    fix: add missing types

---
+++ app/global.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+
+interface Window {
+  ethereum: import("ethers").Eip1193Provider & {
+    on: (event: string, cb: (param: any) => any) => void;
+  };
+  fhevmjs: import("fhevmjs");
+  fhevmjsInitialized: boolean;
+}
